5ebd933e027365d3d9a9bb41e2d19ca15c76844c
Mncedisi Ntshingila <ntshingilamncedisi@gmail.com>
2025-10-05 14:48:49 +0200

    Modified files

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer(){
+const correctAnswer = "4";
+document.querySelector(name);
+let userAnswer = "value";
+
+ if (userAnswer === correctAnswer){
+    feedback.textContent = "Correct! Well done.";
+ }else{
+    feedback.textContent = "That's incorrect. Try again!"
+  }
+
+}
+document.getElementById("submit-answer")
